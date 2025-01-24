bbbf7dc2766b88fa7dc872419ba339dd5e8283a3
MoizKapasi <91624964+MoizKapasi@users.noreply.github.com>
2025-01-24 11:31:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+  <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<BabyAnnouncementsTable />} />
       </Routes>
-    </>
+  </BrowserRouter>
   );
 }
 
