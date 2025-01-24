ab80c4a6c056f300d62eb89f7fc959f2bc4f6366
MoizKapasi <91624964+MoizKapasi@users.noreply.github.com>
2025-01-24 11:44:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  base: '/', // Adjust the base path if necessary
+  build: {
+    outDir: 'dist', // Default output directory
+  },
 });
