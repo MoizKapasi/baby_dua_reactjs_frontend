8e85904e02a4b41168b1798db7a21a744c7718f8
MoizKapasi <91624964+MoizKapasi@users.noreply.github.com>
2025-01-24 11:33:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // Adjust the base path if necessary
+  build: {
+    outDir: 'dist', // Default output directory
+  },
 });
